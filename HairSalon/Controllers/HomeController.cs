cfe8ff289131044edd63637357ad46c1d887fe8a
Jimmy McNamara <jmcnamara44@gmail.com>
2018-05-04 15:34:57 -0700

    let an employee add a new stylist to the database.

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -19,18 +19,30 @@ namespace HairSalon.Controllers
             List<Stylist> allStylists = Stylist.GetAllStylists();
             return View(allStylists);
         }
+        [HttpGet("/onboard-stylist")]
+        public ActionResult AddStylist()
+        {
+            return View();
+        }
         [HttpGet("/all-stylists")]
         public ActionResult AllStylists()
         {
             List<Stylist> allStylists = Stylist.GetAllStylists();
             return View(allStylists);
         }
-
         [HttpPost("/create-client")]
         public ActionResult CreateClient()
         {
             Client newClient = new Client(Request.Form["client-name"], Request.Form["phone-number"], Int32.Parse(Request.Form["stylist-name"]));
             newClient.Save();
+            List<Stylist> allStylists = Stylist.GetAllStylists();
+            return View("AllStylists", allStylists);
+        }
+        [HttpPost("/create-stylist")]
+        public ActionResult CreateStylist()
+        {
+            Stylist newStylist = new Stylist(Request.Form["stylist-name"], Request.Form["days-available"], Int32.Parse(Request.Form["years-active"]), Request.Form["phone-number"]);
+            newStylist.Save();
             List<Stylist> allStylists = Stylist.GetAllStylists();
             return View("AllStylists", allStylists);
         }
